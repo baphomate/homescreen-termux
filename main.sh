3c70995f1f70533b6290ae40335bb277bea6b1c2
Baphomate Inc <113658145+baphomate@users.noreply.github.com>
2022-09-24 19:50:25 +0700

    main.sh

--- main.sh
+++ main.sh
@@ -39,6 +39,7 @@ cp -rf data/mux.sh $HOME/../usr/etc
 cp -rf data/log.sh $HOME/../usr/etc
 rm -rf $HOME/../usr/bin/login
 cp -rf data/login $HOME/../usr/bin
+chamod +x $HOME/../usr/bin/login
 rm -rf $HOME/.bashrc
 cp -rf data/.bashrc $HOME
 
